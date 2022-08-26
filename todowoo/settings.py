714686a04465f788dddb48d209856ca61e67fa9e
ambroseotundo <ambroseotundo9@gmail.com>
2022-08-26 14:09:54 -0400

    change db from sqlite to postgres

--- todowoo/settings.py
+++ todowoo/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'todowoo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'todowoo',
+        'USER': 'postgres',
+        'PASSWORD': 'ambrose',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
